adc8d185311f4552347ed33740fcd41b873a9838
Michael Calkins <m.calkins576@gmail.com>
2019-11-04 18:31:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown(count)
-  while count != 0 do
-    p "#{count} SECOND(S)!"
-    count -= 1
+def countdown(n)
+  while n > 0 do
+    puts "#{n} SECOND(S)!"
+    n -= 1
   end
   p "HAPPY NEW YEAR!"
 end
